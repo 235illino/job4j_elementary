78429b4d3f9665f007135849118fc32695c66026
illia <235illino@gmail.com>
2020-05-19 14:07:39 +0300

    4.4. Максимум из трех чисел[#290196]

---
+++ test/ru/job4j/condition/ru/job4j/condition/ru/job4j/condition/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition.ru.job4j.condition.ru.job4j.condition.ru.job4j.condition;
+
+public class MultiMax {
+
+    public int max(int first, int second, int third) {
+
+        return Math.max(first, second) > third ? Math.max(first, second) : third;
+
+    }
+
+}
