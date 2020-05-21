9d583fe84cf924cc833b3eed452251060598727a
illia <235illino@gmail.com>
2020-05-21 23:37:33 +0300

    6.0.2. Заполнение массива.[226300#290215]

--- src/ru/job4j/condition/ru/job4j/loop/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/condition/ru/job4j/loop/ru/job4j/array/ArrayDefinition.java
@@ -6,6 +6,16 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        String[] names = new String[4];
+        names[0] = "Ol Po";
+        names[1] = "Oll Poo";
+        names[2] = "Pall Cat";
+        names[3] = "Roll Set";
+
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 
 }
