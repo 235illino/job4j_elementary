53b3b2235b084e4be189d1c6d3938cdbbf9b4a0b
illia <235illino@gmail.com>
2020-05-21 16:38:47 +0300

    5.6. Простые числа.[156314#290277]

--- src/ru/job4j/condition/ru/job4j/condition/ru/job4j/condition/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/PrimeNumber.java
+++ src/ru/job4j/condition/ru/job4j/condition/ru/job4j/condition/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/PrimeNumber.java
@@ -1,12 +1,18 @@
 package ru.job4j.condition.ru.job4j.condition.ru.job4j.condition.ru.job4j.loop.ru.job4j.loop.ru.job4j.loop.ru.job4j.loop.ru.job4j.loop.ru.job4j.loop;
 
-//public class PrimeNumber {
+import ru.job4j.condition.ru.job4j.condition.ru.job4j.condition.ru.job4j.loop.ru.job4j.loop.ru.job4j.loop.ru.job4j.loop.ru.job4j.loop.CheckPrimeNumber;
 
-//    public int calc(int finish){
-//        int count = 0;
-//        for (int i = 2; i <=finish; i++){
-//            if ()
-//        }
-//    }
-//
-//}
+public class PrimeNumber {
+
+    public int calc(int finish) {
+        int count = 0;
+        for (int i = 2; i <= finish; i++) {
+            if (CheckPrimeNumber.check(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+}
---
+++ test/ru/job4j/condition/ru/job4j/condition/ru/job4j/condition/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/PrimeNumberTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.condition.ru.job4j.condition.ru.job4j.condition.ru.job4j.loop.ru.job4j.loop.ru.job4j.loop.ru.job4j.loop.ru.job4j.loop.ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PrimeNumberTest {
+
+    @Test
+    public void whenFinish5Primes3() {
+        PrimeNumber primeNumber = new PrimeNumber();
+        int rsl = primeNumber.calc(5);
+        int expected = 3;
+        assertThat(expected, is(rsl));
+    }
+    @Test
+    public void whenFinish5Primes11() {
+        PrimeNumber primeNumber = new PrimeNumber();
+        int rsl = primeNumber.calc(11);
+        int expected = 5;
+        assertThat(expected, is(rsl));
+    }
+    @Test
+    public void whenFinish5Primes2() {
+        PrimeNumber primeNumber = new PrimeNumber();
+        int rsl = primeNumber.calc(2);
+        int expected = 1;
+        assertThat(expected, is(rsl));
+    }
+}
