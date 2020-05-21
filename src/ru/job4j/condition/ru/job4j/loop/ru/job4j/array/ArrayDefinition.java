c6232fdef063eb4e97769bdb141659e47a93edbe
illia <235illino@gmail.com>
2020-05-21 23:40:22 +0300

    6.0.2. Заполнение массива.[226300#290215] version 2.0

--- src/ru/job4j/condition/ru/job4j/loop/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/condition/ru/job4j/loop/ru/job4j/array/ArrayDefinition.java
@@ -13,9 +13,14 @@ public class ArrayDefinition {
         names[2] = "Pall Cat";
         names[3] = "Roll Set";
 
-        for (String name : names) {
-            System.out.println(name);
-        }
+//        for (String name : names) {
+//            System.out.println(name);
+//        }
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 
 }
