a727137e7de226f9b7df03eb7cf816f6c53733f9
illia <235illino@gmail.com>
2020-05-22 23:34:52 +0300

    6.5. Поиск индекса в диапазоне.[156318#290208]

--- test/ru/job4j/condition/ru/job4j/condition/ru/job4j/condition/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/u/job4j/array/ru/job4j/array/FindLoop.java
+++ test/ru/job4j/condition/ru/job4j/condition/ru/job4j/condition/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/u/job4j/array/ru/job4j/array/FindLoop.java
@@ -13,4 +13,18 @@ public class FindLoop {
         return rst;
     }
 
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1;
+
+        for (int index = 0; index <= finish; index++) {
+            if (index >= start) {
+                if (data[index] == el) {
+                    rst = index;
+                    break;
+                }
+            }
+        }
+        return rst;
+    }
+
 }
--- test/ru/job4j/condition/ru/job4j/condition/ru/job4j/condition/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/u/job4j/array/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/condition/ru/job4j/condition/ru/job4j/condition/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/ru/job4j/loop/u/job4j/array/ru/job4j/array/FindLoopTest.java
@@ -19,13 +19,23 @@ public class FindLoopTest {
 
     }
     @Test
-    public void when7IndexMinus1() {
+    public void when8IndexMinus1() {
 
         FindLoop findLoop = new FindLoop();
         int[] input = {5, 6, 7};
         int el = 8;
         int rsl = findLoop.indexOf(input, el);
         int expected = -1;
+        assertThat(expected, is(rsl));
+
+    }
+    @Test
+    public void when14From2To4Index4() {
+
+        int[] input = {5, 6, 7, 8, 14, 0};
+        int el = 14;
+        int rsl = FindLoop.indexOf(input, 14, 2, 4);
+        int expected = 4;
         assertThat(expected, is(rsl));
 
     }
