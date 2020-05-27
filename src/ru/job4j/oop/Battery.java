113436ee1a9ff2e9738c1cd1531968d225cc94dc
illia <235illino@gmail.com>
2020-05-27 20:34:27 +0300

    1.7. Состояние объекта.[167388#290374]

---
+++ src/ru/job4j/oop/Battery.java
@@ -0,0 +1,25 @@
+package ru.job4j.oop;
+
+public class Battery {
+
+    private int value;
+
+    public Battery(int value) {
+        this.value = value;
+    }
+
+    public void exchange(Battery another) {
+        this.value = this.value + another.value;
+        another.value = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(15);
+        Battery second = new Battery(7);
+        System.out.println("first : " + first.value + ". second : " + second.value);
+        first.exchange(second);
+        System.out.println("first : " + first.value + ". second : " + second.value);
+
+    }
+
+}
