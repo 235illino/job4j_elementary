b73a888aab3cc202851aa4e6e6ee675c88d757d8
illia <235illino@gmail.com>
2020-06-01 14:29:01 +0300

    2. Scanner и чтение числа из консоли.[179518#290326]

---
+++ src/io/Matches.java
@@ -0,0 +1,27 @@
+package io;
+
+import java.util.Scanner;
+
+public class Matches {
+
+    public static void main(String[] args) {
+        Scanner input1 = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
+        int sticks = 11;
+        while (sticks > 0) {
+            System.out.println("First player take 1 - 3 sticks");
+            sticks = sticks - Integer.valueOf(input1.nextLine());
+            System.out.println(sticks + " sticks left");
+            if (sticks == 0) {
+                System.out.println("Second player win");
+            }
+            System.out.println("Second player take 1 - 3 sticks");
+            sticks = sticks - Integer.valueOf(input2.nextLine());
+            if (sticks == 0) {
+                System.out.println("First player win");
+            }
+            System.out.println(sticks + " sticks left");
+        }
+    }
+
+}
